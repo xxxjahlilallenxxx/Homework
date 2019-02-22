215d528509c535ddc5139ea809dc0314a766d192
JA@02697400 <jahlil.allen@bison.howard.edu>
2019-02-21 19:26:41 -0500

    Adding a number back to the list so that way I can use sum_list

--- prolog.pl
+++ prolog.pl
@@ -1,9 +1,15 @@
 %Homework Assignment #2
 %Problem #1
-sumUpNumbersSimple(L, N):-
+sum_up_numbers_general(L, N) :-
   [X|Y] = L,
-  Z is 0,
-  N is Z + X,
-  sumUpNumbersSimple(Y, N).
+  number(X),
+  L1 = add_back_to_list(Y, X, R),
+  sum_up_numbers_general(L1, R),
+  sum_list(L1).
+
+add_back_to_list(A, X, R) :-
+  append(A, X, R).
   %When I ran sum-up-numbers-simple([100,200], 0) the simulator returned back
   %false.
+
+%Problem #2
