296867e2e81bc4a567a101157a73f02e1e1d0990
JA@02697400 <jahlil.allen@bison.howard.edu>
2019-02-22 10:24:37 -0500

    Started the problem #2

--- prolog.pl
+++ prolog.pl
@@ -1,11 +1,13 @@
 %Homework Assignment #2
 %Problem #1
-sum_up_numbers_general(L, N) :-
+sum_up_numbers_simple(L, N) :-
   [X|Y] = L,
   number(X),
   L1 = add_back_to_list(Y, X, R),
   sum_up_numbers_general(L1, R),
-  sum_list(L1).
+  N is X + sum_list(L1);
+  sum_up_numbers_general(Y, R);
+  N is X + sum_list(L1).
 
 add_back_to_list(A, X, R) :-
   append(A, X, R).
@@ -13,3 +15,10 @@ add_back_to_list(A, X, R) :-
   %false.
 
 %Problem #2
+sum_up_numbers_general(L, N) :-
+  [X|Y] = L,
+  is_list(X),
+  length(X, R1) =:= 0,
+  add_back_to_list(R1, Y, R2)
+  sum_up_numbers_general(R2, R3);
+  
